eac583cbf36e9bda7050161cf1bd65ccf2d99a3d
Herman <herman.holterman@gmail.com>
2019-11-05 22:02:40 +0100

    compare dicts -> nested, todo: lists

--- ijr/generic_lib.py
+++ ijr/generic_lib.py
@@ -2,17 +2,28 @@ import datetime
 import os
 
 
-def dict_compare(old_dict, new_dict):
+def dict_compare(old_dict, new_dict, nested=None):
     """ Compare two dictionaries
         Only 1 level, ignoring attributes starting with '_'
     """
+    key_prefix = nested + '.' if nested else ''
     intersect_keys = old_dict.keys() & new_dict.keys()
-    modified = {k: dict(old=old_dict[k], new=new_dict[k], action='mod')
-                    for k in intersect_keys if k[0] != '_' and old_dict[k] != new_dict[k]}
+    modified = {key_prefix + k: dict(old=old_dict[k], new=new_dict[k], action='mod') for k in intersect_keys
+                        if k[0] != '_'
+                        and old_dict[k] != new_dict[k]
+                        and not (isinstance(old_dict[k], dict) and isinstance(new_dict[k], dict))}
+
+    nested_keys = [k for k in intersect_keys if k[0] != '_' and isinstance(old_dict[k], dict) and isinstance(new_dict[k], dict)]
+    for k in nested_keys:
+        x = dict_compare(old_dict[k], new_dict[k], key_prefix + k)
+        if x:
+            modified.update(x)
+
     added = new_dict.keys() - old_dict.keys()
-    modified.update({k: dict(new=new_dict[k], action='add') for k in added if k[0] != '_'})
+    modified.update({key_prefix + k: dict(new=new_dict[k], action='add') for k in added if k[0] != '_'})
+
     deleted = old_dict.keys() - new_dict.keys()
-    modified.update({k: dict(old=old_dict[k], action='del') for k in deleted if k[0] != '_'})
+    modified.update({key_prefix + k: dict(old=old_dict[k], action='del') for k in deleted if k[0] != '_'})
     if modified:
         return modified
 
