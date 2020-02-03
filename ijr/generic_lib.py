df2474f5f6352737d7a405f85d1d7f1ec0326732
Herman <herman.holterman@gmail.com>
2020-02-03 12:39:55 +0100

    "|" instead of "." for nested attributes -> MongoDB

--- ijr/generic_lib.py
+++ ijr/generic_lib.py
@@ -6,7 +6,7 @@ def dict_compare(old_dict, new_dict, nested=None):
     """ Compare two dictionaries
         Only 1 level, ignoring attributes starting with '_'
     """
-    key_prefix = nested + '.' if nested else ''
+    key_prefix = nested + '|' if nested else ''
     intersect_keys = old_dict.keys() & new_dict.keys()
     modified = {key_prefix + k: dict(old=old_dict[k], new=new_dict[k], action='mod') for k in intersect_keys
                         if k[0] != '_'
@@ -39,3 +39,31 @@ def default_object(o):
     if isinstance(o, (datetime.date, datetime.datetime)):
         return o.isoformat()
     raise TypeError("Type %s not serializable" % type(o))
+
+
+# def buid_dict(composed_key, key_value):
+#     def recursive_buid_dict(keys, value, nest_level=0):
+#         if nest_level < len(keys):
+#             return recursive_buid_dict(keys, value={keys[nest_level]: value}, nest_level=nest_level + 1)
+#         elif nest_level == len(keys):
+#             return value
+#
+#     return recursive_buid_dict(keys=list(reversed(composed_key.split('.'))), value=key_value)
+#
+# from collections import ChainMap
+#
+# def merge_dicts(dict_a, dict_b):
+#     new_dict = dict()
+#     for key in ChainMap(dict_a, dict_b):
+#         val_a , val_b = dict_a.get(key), dict_b.get(key)
+#         if isinstance(val_a, dict) and isinstance(val_b, dict):
+#             new_dict[key] = merge_dicts(val_a, val_b)
+#         elif val_a is None and val_b is None:
+#             continue
+#         elif val_a is None:
+#             new_dict[key] = val_b
+#         elif val_b is None or val_a == val_b:
+#             new_dict[key] = val_a
+#         else:
+#             new_dict[key] = [val_a, val_b]
+#     return new_dict
